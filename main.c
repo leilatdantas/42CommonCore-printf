356a2a6b188b054f239e0c8feb51fe5d991b8f9f
Leila Dantas <leiladantas@MacBook-Air.lan>
2023-05-10 19:34:32 +0100

    updates

--- main.c
+++ main.c
@@ -3,25 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lebarbos <lebarbos@student.42porto.com     +#+  +:+       +#+        */
+/*   By: lebarbos <lebarbos@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 18:48:24 by lebarbos          #+#    #+#             */
-/*   Updated: 2023/05/10 13:05:06 by lebarbos         ###   ########.fr       */
+/*   Updated: 2023/05/10 18:41:19 by lebarbos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "ft_printf.h"
-// #include <limits.h>
-
+#include "ft_printf.h"
+#include <limits.h>
 #include<stdio.h>
 
 int	main(void)
 {
-	int					*ponteiro;
-	unsigned long long	nb;
+	int	*ptr;
 
-	nb = 12;
-	printf("\n\nUNSIGNED LONG LONG: %lu\nPONTEIRO: %lu\n\n", nb, sizeof(ponteiro));
-	ft_printf ("isso é um inteiro: %d, isso é uma string: %s", 2147483648,
-		"ola tudo bem?");
+	ptr = 0;
+	printf("%p\n", ptr);
+	ft_printf ("%p\n", ptr);
 }
